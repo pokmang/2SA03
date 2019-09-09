6bcf0671f20142f332b5e602ae78a0b258cd0166
usman <5935512036@psu.ac.th>
2019-09-09 15:30:35 +0700

    hello

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Content from './Content'
-
-
 class App extends Component {
  render() {
-  return (
-    <div className="App">
-       Hello MADUProductin SaLAM
-       <Content/>
-    </div>
+ return (
+ <div className="App">
+ Hello World
+ </div>
  );
  }
 }
