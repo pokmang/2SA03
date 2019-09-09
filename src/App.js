20de13e85a17cece8c552b8b664ca005abcb0a67
usman <5935512036@psu.ac.th>
2019-09-09 15:54:59 +0700

    Conponent&prop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import CharacterCard from './CharacterCard';
+import React,{Component} from 'react';
 import './App.css';
 class App extends Component {
  render() {
  return (
- <div className="App">
- Hello World
+ <div>
+ <CharacterCard value="h"/>
+<CharacterCard value="i"/>
  </div>
  );
  }
 }
-export default App;
+export default App
