f8a695031d03917612e6a0fcb257bb4f8fce221e
Pokmang <34453637+5935512036@users.noreply.github.com>
2019-09-18 20:19:24 +0700

    last

--- src/Start.js
+++ src/Start.js
@@ -8,8 +8,8 @@ function Start() {
   return (
     <div className="one">
       <div className="row">
-        <h1 className="text-head">Let's play my first game on react</h1>
-        <div><h1 className="color">(js)</h1></div>
+        <h1 className="text-head"></h1>
+        <div><h1 className="color">เกมใบ้คำ พัฒนาโดย React</h1></div>
       </div>
     </div>
   );
